e2e165d90a9dba89831dd39d96944f3fe2406e72
goodmayonnaise <jeonjiy2on@gmail.com>
2022-01-19 20:27:38 +0900

    edit name

---
+++ projects/SentimentClassification/datadownloader.py
@@ -0,0 +1,8 @@
+# 데이터 다운로드 코드 
+# pip install urllib
+# python datadownloader.py 
+
+import urllib.request
+
+urllib.request.urlretrieve("https://raw.githubusercontent.com/e9t/nsmc/master/ratings_train.txt", filename="ratings_train.txt")
+urllib.request.urlretrieve("https://raw.githubusercontent.com/e9t/nsmc/master/ratings_test.txt", filename="ratings_test.txt")
